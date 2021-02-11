9d1e67e5e73fbc84c18b7509c99eb6589d447b4c
Igor Escodro <escodro@outlook.com>
2021-02-11 13:51:40 -0300

    Second interaction with Android Guild

---
+++ app/src/main/java/com/example/dexbank/CardType.kt
@@ -0,0 +1,33 @@
+package com.example.dexbank
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.ui.graphics.vector.ImageVector
+
+enum class CardType(
+    val icon: ImageVector,
+    val title: String,
+    val body: String,
+    val value: String
+) {
+    CreditCard(
+        icon = Icons.Outlined.ShoppingCart,
+        title = "Cartão de Crédito",
+        body = "Fatura atual",
+        value = "R$ 756,46"
+    ),
+    Account(
+        icon = Icons.Outlined.Info,
+        title = "Conta corrente",
+        body = "Saldo disponível",
+        value = "R$ 88.000,98"
+    ),
+    Loan(
+        icon = Icons.Outlined.CheckCircle,
+        title = "Empréstimo",
+        body = "Valor dispnonível",
+        value = "R$ 200.000,00"
+    )
+}
--- app/src/main/java/com/example/dexbank/MainActivity.kt
+++ app/src/main/java/com/example/dexbank/MainActivity.kt
@@ -2,27 +2,36 @@ package com.example.dexbank
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.preferredHeight
 import androidx.compose.foundation.layout.preferredSize
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
-import androidx.compose.material.Surface
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.dexbank.ui.theme.DexBankTheme
@@ -32,16 +41,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DexBankTheme {
-                Surface(color = Color.Transparent) {
-                    DexMainCard()
-                }
+                Scaffold(
+                    backgroundColor = MaterialTheme.colors.primary,
+                    topBar = { DexToolbar() },
+                    bodyContent = {
+                        val list = CardType.values().toList()
+                        DexMainList(cardList = list)
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun DexMainCard() {
+fun DexMainList(cardList: List<CardType>) {
+    LazyColumnFor(items = cardList) { card ->
+        DexMainCard(cardType = card)
+    }
+}
+
+@Composable
+fun DexMainCard(cardType: CardType) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -56,29 +77,71 @@ fun DexMainCard() {
         ) {
             Row {
                 Icon(
-                    imageVector = Icons.Outlined.Notifications,
+                    imageVector = cardType.icon,
                     modifier = Modifier.preferredSize(32.dp)
                 )
                 Spacer(modifier = Modifier.width(12.dp))
                 Text(
-                    text = "Cartão de Crédito",
-                    modifier = Modifier.align(Alignment.CenterVertically)
+                    text = cardType.title,
+                    modifier = Modifier.align(Alignment.CenterVertically),
+                    style = MaterialTheme.typography.body1
                 )
             }
-            Surface(modifier = Modifier.padding(top = 8.dp)) {
-                Text(text = "Fatura atual")
-            }
-            Surface(modifier = Modifier.padding(top = 12.dp)) {
-                Text(text = "R$ 756,98")
-            }
+            Text(
+                text = cardType.body,
+                style = MaterialTheme.typography.body2,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+            Text(
+                text = cardType.value,
+                style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.padding(top = 12.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun DexToolbar() {
+    TopAppBar(modifier = Modifier.preferredHeight(100.dp)) {
+        Row(modifier = Modifier.fillMaxHeight(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = "Olá, Guilda",
+                style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold)
+            )
+            DexToolbarButton()
+            DexToolbarButton()
+        }
+    }
+}
+
+@Composable
+fun DexToolbarButton() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .preferredSize(56.dp)
+            .clip(CircleShape)
+            .background(MaterialTheme.colors.primaryVariant)
+    ) {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Outlined.Notifications)
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun MainCardPreview() {
     DexBankTheme {
-        DexMainCard()
+        DexMainCard(CardType.Account)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToolbarPreview() {
+    DexBankTheme {
+        DexToolbar()
     }
 }
